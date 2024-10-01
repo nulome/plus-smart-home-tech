bbde70fab98d399a2b7ccb0f29098824102a22a5
HP <Mazer30@yandex.ru>
2024-10-01 23:44:59 +0400

    feat: proto + grpc

--- telemetry/collector/src/main/java/collect/mapper/HubEventMapper.java
+++ telemetry/collector/src/main/java/collect/mapper/HubEventMapper.java
@@ -3,10 +3,17 @@ package collect.mapper;
 
 import collect.model.device.DeviceAction;
 import collect.model.device.DeviceAddedEvent;
+import collect.model.device.DeviceRemovedEvent;
 import collect.model.device.DeviceType;
+import collect.model.scenario.ScenarioAddedEvent;
 import collect.model.scenario.ScenarioCondition;
+import collect.model.scenario.ScenarioRemovedEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceAddedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceRemovedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioAddedEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.ScenarioRemovedEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceActionAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceTypeAvro;
@@ -19,17 +26,16 @@ public abstract class HubEventMapper {
 
     public abstract DeviceActionAvro toDeviceActionAvro(DeviceAction deviceAction);
 
+    public abstract DeviceAddedEvent toHubEvent(DeviceAddedEventProto eventProto);
+    public abstract DeviceRemovedEvent toHubEvent(DeviceRemovedEventProto eventProto);
+    public abstract ScenarioAddedEvent toHubEvent(ScenarioAddedEventProto eventProto);
+    public abstract ScenarioRemovedEvent toHubEvent(ScenarioRemovedEventProto eventProto);
+
     @Mapping(target = "type", expression = "java(this.getDeviceTypeAvro(addedEvent.getDeviceType()))")
     public abstract DeviceAddedEventAvro toDeviceAddedEventAvro(DeviceAddedEvent addedEvent);
 
 
     DeviceTypeAvro getDeviceTypeAvro(DeviceType type) {
-        return switch (type) {
-            case CLIMATE_SENSOR -> DeviceTypeAvro.CLIMATE_SENSOR;
-            case LIGHT_SENSOR -> DeviceTypeAvro.LIGHT_SENSOR;
-            case MOTION_SENSOR -> DeviceTypeAvro.MOTION_SENSOR;
-            case SWITCH_SENSOR -> DeviceTypeAvro.SWITCH_SENSOR;
-            case TEMPERATURE_SENSOR -> DeviceTypeAvro.TEMPERATURE_SENSOR;
-        };
+        return DeviceTypeAvro.valueOf(type.name());
     }
 }
